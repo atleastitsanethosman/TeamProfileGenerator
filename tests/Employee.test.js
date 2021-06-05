3f0b5c0656b2afbdfde8d78b25b92034ec5ba831
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-05 15:38:43 -0400

    fixed issue with jest script in package.json

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
-describe('Employee class', () =>{
+describe('Employee class', () => { 
     it("New employee returns a role of employee", () =>{
         expect(new Employee('Greg').getRole().toBe('Employee'))
     });
