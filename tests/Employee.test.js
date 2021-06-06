c0b6078e920e43ff0f4f113b991d8ee958fd8872
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-05 22:29:09 -0400

    fixed jest issue, created tests for employee cclass and built out class to pass.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,8 +1,22 @@
 const Employee = require("../lib/Employee");
 
 describe('Employee class', () => { 
-    it("New employee returns a role of employee", () =>{
-        expect(new Employee('Greg').getRole().toBe('Employee'))
-    });
+    // constants for employee to test.
+    const eName = 'Ian Hennessey';
+    const eId = '0004'
+    const eRole = 'Manager'
     
+    it("New employee returns a role of employee", () =>{
+        expect(new Employee(eName, eId, eRole).getRole()).toBe('Employee')
+    });
+    it("New Employee returns a name in all uppercase", () => {
+        expect(new Employee(eName, eId, eRole).getName()).toBe(eName.toUpperCase())
+    });
+    it("Gives employee ID matching input", () => {
+        expect(new Employee(eName, eId, eRole).getId()).toBe(eId)
+    });
+    it("Populates email address using convention of no spaces and all lowercase characters", () =>{
+        expect(new Employee(eName, eId, eRole).getEmail()).toBe('ianhennessey@fakeemail.com')
+    });
+
 })
