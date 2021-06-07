3120cb8a00208bda4cdd6c4546f7243f2353d80a
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-07 14:41:33 -0400

    fixing employee tests and class based on re-reading use case

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,19 +4,19 @@ describe('Employee class', () => {
     // constants for employee to test.
     const eName = 'Ian Hennessey';
     const eId = '0004'
-    const eRole = 'Manager'
+    const eMail = 'ianhennessey@fakeemail.com'
     
     it("New employee returns a role of employee", () =>{
-        expect(new Employee(eName, eId, eRole).getRole()).toBe('Employee')
+        expect(new Employee(eName, eId, eMail).getRole()).toBe('Employee')
     });
     it("New Employee returns a name in all uppercase", () => {
-        expect(new Employee(eName, eId, eRole).getName()).toBe(eName.toUpperCase())
+        expect(new Employee(eName, eId, eMail).getName()).toBe(eName.toUpperCase())
     });
     it("Gives employee ID matching input", () => {
-        expect(new Employee(eName, eId, eRole).getId()).toBe(eId)
+        expect(new Employee(eName, eId, eMail).getId()).toBe(eId)
     });
-    it("Populates email address using convention of no spaces and all lowercase characters", () =>{
-        expect(new Employee(eName, eId, eRole).getEmail()).toBe('ianhennessey@fakeemail.com')
+    it("Populates email address as passed to function", () =>{
+        expect(new Employee(eName, eId, eMail).getEmail()).toBe('ianhennessey@fakeemail.com')
     });
 
 })
